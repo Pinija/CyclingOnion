a4302b816e8c779a3cf77ae7db5efb95d60debfb
Pinija <t.he@arcor.de>
2025-10-27 23:42:29 +0100

    Renamed variables - removed uneccessary code

--- outfitter.py
+++ outfitter.py
@@ -1,12 +1,13 @@
-from weather import WeatherForecast, get_weather_forecast
+from weather import WeatherForecast
 from wardrobe import get_clothing_wardrobe
+from properties import Intensity, Terrain
 
 
-def get_optimized_outfit(hours: int, weather: WeatherForecast):
+def get_optimized_outfit(hours: int, weather_fc: WeatherForecast, intensity: Intensity, terrain: Terrain):
     """Minimize the discomfort score on all possible clothing sets for the current weather prediction."""
-    eff_temp_min, eff_temp_max = weather.get_effective_temp_range()
-    wind_max = weather.get_wind_max()
-    precip_prob = weather.get_precipitation_prob()
+    eff_temp_min, eff_temp_max = weather_fc.get_effective_temp_range()
+    wind_max = weather_fc.get_wind_max()
+    precip_prob = weather_fc.get_precipitation_prob()
 
     wardrobe = get_clothing_wardrobe()
     combos = {
@@ -19,19 +20,11 @@ def get_optimized_outfit(hours: int, weather: WeatherForecast):
 
     outfit = {}
     for name, combo in combos.items():
-        combo.sort(key=lambda x: x.get_discomfort(eff_temp_min, eff_temp_max, precip_prob, wind_max, hours))
+        combo.sort(key=lambda x: x.get_discomfort(eff_temp_min, eff_temp_max, precip_prob, wind_max, hours, intensity, terrain))
         outfit[name] = combo[0]
     
     return outfit
 
 if __name__ == "__main__":
-    params = ["Freiburg", 3, "mountain", "hard"]
-    weather = get_weather_forecast(*params)
-    effective_weather = weather.get_effective_temp_range()
-    print(weather)
-    print("Effective temp (min, max): " + str(effective_weather))
-    outfit = get_optimized_outfit(3, weather)
-    for body_part, combo in outfit.items():
-        print(body_part + ": " + str(combo))
-
+    pass
     
